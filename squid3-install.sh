4e64281ae3527508eb68cedbacd333351addb0c8
Leoh <73969367+leohturbo@users.noreply.github.com>
2021-03-29 04:38:56 -0300

    Update files

--- squid3-install.sh
+++ squid3-install.sh
@@ -1,36 +1,16 @@
 echo "Installing..."
+
+if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 20.04"; then
     /usr/bin/apt update
     /usr/bin/apt -y install apache2-utils squid3
     touch /etc/squid/passwd
-    /bin/rm -f /etc/squid/squidleoh.conf
+    /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squidleoh.conf https://raw.githubusercontent.com/leohturbo/squid/master/squidleoh.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/leohturbo/squid/master/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
     /sbin/iptables-save
     service squid restart
     systemctl enable squid
-echo "Installed! =)"
-
-echo "Running IP updates."
-
-IP_ALL=$(/sbin/ip -4 -o addr show scope global | awk '{gsub(/\/.*/,"",$4); print $4}')
-
-IP_ALL_ARRAY=($IP_ALL)
-
-SQUID_CONFIG="\n"
-
-for IP_ADDR in ${IP_ALL_ARRAY[@]}; do
-    ACL_NAME="proxy_ip_${IP_ADDR//\./_}"
-    SQUID_CONFIG+="acl ${ACL_NAME}  myip ${IP_ADDR}\n"
-    SQUID_CONFIG+="tcp_outgoing_address ${IP_ADDR} ${ACL_NAME}\n\n"
-done
-
-echo "Updating squid config"
-
-echo -e $SQUID_CONFIG >> /etc/squid/squid.conf
-
-echo "Restarting squid..."
-
-systemctl restart squid
-
-echo "Done"
+else
+    echo "OS NOT SUPPORTED.\n"
+    exit 1;
